9b2da22c415af70c756ad23bd4b22ba8befe442b
Lyndon Mackay <lyndondmackay@gmail.com>
2018-04-13 14:40:18 +1000

    Added info for generals sortable and removed the hover from non-sortable columns

--- src/app/generals/generals.component.ts
+++ src/app/generals/generals.component.ts
@@ -10,15 +10,40 @@ import { General } from '../general';
 export class GeneralsComponent implements OnInit {
 
   generals:General[];
+  private reverse = false;
   constructor(private generalService:GeneralService) { }
 
   ngOnInit() {
     this.getGenerals();
   }
- 
-
+  countryClicked():void
+  {
+    this.reverse = !this.reverse;
+    this.generals.sort((a,b) => this.sort(a,b,"Country",this.reverse));
+  }
   getGenerals(): void {
     this.generalService.getGenerals()
         .subscribe(generals => this.generals = generals);
+  }
+  nameClicked():void{
+    this.reverse = !this.reverse;
+    this.generals.sort((a,b) => this.sort(a,b,"Name",this.reverse));
+  }
+  sort(a: General, b: General,property:string, reverse : boolean): number {
+
+    if (a[property] === b[property]) {
+      return 0;
+    }
+    let rValue = 0;
+    if (a[property] > b[property]) {
+      rValue = 1;
+    }
+    else {
+      rValue = -1;
+    }
+    if (this.reverse) {
+      rValue *= -1;
+    }
+    return rValue;
   }
 }
